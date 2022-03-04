5cc72f5b27d3b649a6757a2ba4b6493972e51c4b
nicolas-livingston <lucass.introzzi@gmail.com>
2022-03-04 11:06:20 -0300

    parte del responsive hecho

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import FormButton from '../FormButton/FormButton'
 import './Navbar.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     return (
         <div className='navbar'>
+            <div className='container-mobile'>
+                <div className='menu-mobile'>
+                    <a href='#test' className='logo-responsive'>
+                        <img alt="logo" src='/logo-provisorio.png'></img>
+                    </a>
+                    <a href='#test' className='burger-menu'>
+                        <FontAwesomeIcon icon={faBars} />
+                    </a>
+                </div>
+            </div>
             <div className='container'>
                 <div className='navbar-list'>
                     <div className='nav-logo'>
                         <a href='#test'>
                             <img alt="logo" src='/logo-provisorio.png'></img>
                         </a>
+
                     </div>
                     <div className='nav-menu'>
                         <a href='#test'>Work</a>
@@ -19,6 +32,7 @@ const Navbar = () => {
                         <a href='#test'>Community</a>
                     </div>
                 </div>
+
                 <FormButton>Lets talk!</FormButton>
             </div>
         </div>
